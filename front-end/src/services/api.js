c1a7b0024d40cbe829aeccc1ccb45fa99de991af
ahllyy <amanda.halley@keyin.com>
2025-04-13 15:26:08 -0230

    api service for all backend routes

---
+++ front-end/src/services/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+//handling api calls to backend
+const API_BASE_URL = 'http://localhost:8082'; // keep in mind this is on port 8082
+
+//get endpoints
+export const fetchCities = () => axios.get(`${API_BASE_URL}/city`);
+export const fetchAirports = () => axios.get(`${API_BASE_URL}/airport`);
+export const fetchAircraft = () => axios.get(`${API_BASE_URL}/aircraft`);
+export const fetchPassengers = () => axios.get(`${API_BASE_URL}/passenger`);
+
+//relationship endpoints
+export const getAirportsInCity = (cityId) =>
+  axios.get(`${API_BASE_URL}/city/${cityId}/airports`);
+
+export const getAircraftByPassenger = (passengerId) =>
+  axios.get(`${API_BASE_URL}/passenger/${passengerId}/aircraft`);
+
+export const getAirportsByPassenger = (passengerId) =>
+  axios.get(`${API_BASE_URL}/passenger/${passengerId}/airports`);
+
+export const getAirportsByAircraft = (aircraftId) =>
+  axios.get(`${API_BASE_URL}/aircraft/${aircraftId}/airports`);
