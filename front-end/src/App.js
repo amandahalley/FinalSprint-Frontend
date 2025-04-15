004c90227f081dc1e57343dbf3ea10255b2aaac7
Tyler Wall <Tyler.wall@keyin.com>
2025-04-15 19:28:48 -0230

    code for App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,2 +1,36 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+
+import AircraftForm from "./components/Aircrafts/AircraftForm";
+import AircraftList from "./components/Aircrafts/AircraftList";
+import AirportForm from "./components/Airports/AirportForm";
+import AirportList from "./components/Airports/AirportList";
+import CityForm from "./components/City/CityForm";
+import CityList from "./components/City/CityList";
+import PassengerForm from "./components/Passenger/PassengerForm";
+import PassengerList from "./components/Passenger/PassengerList";
+
+const App = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Final Sprint Flight Dashboard</h1>
+
+      <h2>Aircrafts</h2>
+      <AircraftForm />
+      <AircraftList />
+
+      <h2>Airports</h2>
+      <AirportForm />
+      <AirportList />
+
+      <h2>Cities</h2>
+      <CityForm />
+      <CityList />
+
+      <h2>Passengers</h2>
+      <PassengerForm />
+      <PassengerList />
+    </div>
+  );
+};
+
+export default App;
