3e7cb8a73a221ff91e17b13072edbe4646ec5719
Tyler Wall <Tyler.wall@keyin.com>
2025-04-16 10:47:14 -0230

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import AircraftForm from "./components/Aircrafts/AircraftForm";
-import AircraftList from "./components/Aircrafts/AircraftList";
-import AirportForm from "./components/Airports/AirportForm";
-import AirportList from "./components/Airports/AirportList";
-import CityForm from "./components/City/CityForm";
-import CityList from "./components/City/CityList";
-import PassengerForm from "./components/Passenger/PassengerForm";
-import PassengerList from "./components/Passenger/PassengerList";
+import HomePage from "./components/pages/HomePage";
+import AircraftList from "./components/pages/AircraftList";
+import AirportList from "./components/pages/AirportList";
+import CityList from "./components/pages/CityList";
+import PassengerList from "./components/pages/PassengerList";
 
 const App = () => {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Final Sprint Flight Dashboard</h1>
-
-      <h2>Aircrafts</h2>
-      <AircraftForm />
-      <AircraftList />
-
-      <h2>Airports</h2>
-      <AirportForm />
-      <AirportList />
-
-      <h2>Cities</h2>
-      <CityForm />
-      <CityList />
-
-      <h2>Passengers</h2>
-      <PassengerForm />
-      <PassengerList />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/aircrafts" element={<AircraftList />} />
+        <Route path="/airports" element={<AirportList />} />
+        <Route path="/cities" element={<CityList />} />
+        <Route path="/passengers" element={<PassengerList />} />
+      </Routes>
+    </Router>
   );
 };
 
